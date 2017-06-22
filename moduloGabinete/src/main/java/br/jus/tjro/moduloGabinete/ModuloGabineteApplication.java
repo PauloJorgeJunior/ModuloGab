e854aec14485fd675098e6d7e2552371f217ae01
Paulo Jorge Ferreira do Nascimento Junior <paulojorge@tjro.jus.br>
2017-06-22 13:02:54 -0400

    Initial commit

---
+++ moduloGabinete/src/main/java/br/jus/tjro/moduloGabinete/ModuloGabineteApplication.java
@@ -0,0 +1,12 @@
+package br.jus.tjro.moduloGabinete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloGabineteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloGabineteApplication.class, args);
+	}
+}
